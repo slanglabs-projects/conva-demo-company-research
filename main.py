4e6628da09d627a44897df6fbe4b950ae0edd55d
Giridhar Murthy <gmurthy@slanglabs.in>
2024-08-27 21:52:09 +0530

    [Conva Demo] Implement e2e functionality

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 from bs4 import BeautifulSoup
+from cacheout import lru_memoize
 from conva_ai import ConvaAI
 from fake_useragent import UserAgent
 from playwright.sync_api import sync_playwright
 import streamlit as st
 import requests
+import re
 
-st.title("AI Competitor Analyst")
-st.caption("Powered by Conva.AI")
-st.divider()
+st.set_page_config(page_title="AI Competitor Analyst - by Conva.AI")
+
+SEARCH_SUFFIXES = [
+    "",
+    "company metrics",
+    "mission",
+    "vision",
+    "milestones",
+    "financial performance",
+    "products and services",
+    "funding",
+]
+
+BING_SEARCH_API_KEY = st.secrets.bing_api_key
+
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
 
 
+def escape_braces(text: str) -> str:
+    text = re.sub(r"(?<!\{)\{(?!\{)", r"{{", text)  # noqa
+    text = re.sub(r"(?<!\})\}(?!\})", r"}}", text)  # noqa
+    return text
+
+
+@lru_memoize()
 def scrape(url: str):
     try:
         with sync_playwright() as p:
@@ -45,97 +73,168 @@ def scrape(url: str):
         return ""
 
 
-def execute():
-    if not st.session_state.company_id:
-        return
+def get_md_normal_text(text):
+    return "<p> {} </p>".format(text)
 
-    suffixes = [
-        "",
-        "company metrics",
-        "mission",
-        "vision",
-        "milestones",
-        "financial performance",
-        "products and services",
-        "funding",
-    ]
-    headers = {"Ocp-Apim-Subscription-Key": "39798b31fbfa426e9d4cb03f8200f1a7"}
+
+def get_md_list(arr):
+    lis = ""
+    for elem in arr:
+        if "$" in elem:
+            elem = elem.replace("$", "\\$")
+        lis += "<li> {} </li>".format(elem)
+    return "<list> {} </list>".format(lis)
+
+
+if "ready" not in st.session_state:
+    st.session_state.ready = False
+
+if "success" not in st.session_state:
+    st.session_state.success = False
+
+if "reset" not in st.session_state:
+    st.session_state.reset = True
+
+st.image("conva.ai.svg", width=100)
+st.title("AI Competitor Analyst")
+
+with st.container(border=True):
+    st.text_input("Company name or URL", key="company_id")
+    col1, col2, col3 = st.columns([2, 2, 4])
+    st.session_state.ready = col1.button("Generate Report")
+    st.session_state.reset = col2.button("Reset")
+    pbph = col3.empty()
+
+rph = st.empty()
+response = None
+
+if st.session_state.reset:
+    pbph.empty()
+    rph.empty()
+    st.session_state.ready = False
+    st.session_state.success = False
+
+if st.session_state.ready and st.session_state.company_id:
+    progress = 5
+    pb = pbph.progress(progress, "Gathering relevant information...")
 
     all_urls = []
-    for suffix in suffixes:
+    headers = {"Ocp-Apim-Subscription-Key": BING_SEARCH_API_KEY}
+    for suffix in SEARCH_SUFFIXES:
         params = {
             "q": st.session_state.company_id + " " + suffix,
             "textFormat": "HTML",
             "count": 5,
         }
         response = requests.get(
-            "https://api.bing.microsoft.com/v7.0/search", headers=headers, params=params
+            "https://api.bing.microsoft.com/v7.0/search",
+            headers=headers,
+            params=params,
         )
         response.raise_for_status()
         search_results = response.json()
         for sr in search_results["webPages"]["value"]:
             url = sr["url"]
-            if url not in all_urls and not "youtube" in url:
+            if url not in all_urls and "youtube" not in url:
                 all_urls.append(url)
 
+    progress += 5
+    pb.progress(progress, "Processing search results...")
+
     context = ""
+    suffix = ""
+
+    pdelta = int(80 / len(all_urls))
+    sdelta = int(len(all_urls) / (len(SEARCH_SUFFIXES) - 1))
     for index, url in enumerate(all_urls):
-        print("Processing URL {} of {} [{}]".format(index, len(all_urls), url))
-        st.text("Processing URL {} of {} [{}]".format(index, len(all_urls), url))
+        progress += pdelta
+        if index % sdelta == 0:
+            sindex = int(index / sdelta) + 1
+            sindex = min(sindex, len(SEARCH_SUFFIXES) - 1)
+            suffix = SEARCH_SUFFIXES[sindex]
+        prefix = "Researching" if "company" in suffix else "Researching company"
+        pb.progress(progress, "{} {}...".format(prefix, suffix))
         content = scrape(url)
         context += "\n\n" + content
 
     client = ConvaAI(
-        assistant_id="8bbc5fba6a6f4e2db83ac9d7a806730b",
-        api_key="68e509b36d54471e8433d06f09a3207b",
-        assistant_version="4.0.0",
+        assistant_id=st.secrets.conva_assistant_id,
+        api_key=st.secrets.conva_api_key,
+        assistant_version="5.0.0",
     )
 
-    streaming_response = client.invoke_capability_name(
-        query="Generate a report for the given company. \n\nCompany Details: {}".format(
-            context
+    progress += 5
+    pb.progress(progress, "Generating report...")
+    capability_context = {"company_report_generation": escape_braces(context).strip()}
+
+    response = client.invoke_capability_name(
+        query="Generate a detailed report for the company whose details are provided. ({})".format(
+            st.session_state.company_id
         ),
         capability_name="company_report_generation",
         timeout=600,
-        stream=True,
+        stream=False,
+        capability_context=capability_context,
     )
 
-    response = None
+    pb.progress(100, "Completed")
+    st.session_state.success = True
 
-    for sr in streaming_response:
-        print(sr)
-        response = sr
+if st.session_state.success:
+    with rph.container(border=True):
+        st.header("Summary and Key Insights")
+        st.markdown(
+            get_md_normal_text(
+                response.parameters.get("summary_and_key_insights", "Unavailable")
+            ),
+            unsafe_allow_html=True,
+        )
 
-    print("Final Response: " + str(response))
-    st.header("Summary and Key Insights")
-    st.text(response.parameters.get("summary_and_key_insights"), "Unavailable")
+        st.header("Company Overview")
+        st.markdown(
+            get_md_normal_text(
+                response.parameters.get("company_overview", "Unavailable")
+            ),
+            unsafe_allow_html=True,
+        )
 
-    st.header("Company Overview")
-    st.text(response.parameters.get("company_overview", "Unavailable"))
+        st.header("Company Metrics")
+        metrics = response.parameters.get("company_metrics", [])
+        st.markdown(get_md_list(metrics), unsafe_allow_html=True)
 
-    st.header("Company Metrics")
-    metrics = response.parameters.get("company_metrics", [])
-    st.text("\n".join(metrics))
+        st.header("Company Mission")
+        st.markdown(
+            get_md_normal_text(response.parameters.get("mission", "Unavailable")),
+            unsafe_allow_html=True,
+        )
 
-    st.header("Company Mission")
-    st.text(response.parameters.get("mission", "Unavailable"))
+        st.header("Company Vision")
+        st.markdown(
+            get_md_normal_text(response.parameters.get("vision", "Unavailable")),
+            unsafe_allow_html=True,
+        )
 
-    st.header("Company Vision")
-    st.text(response.parameters.get("vision", "Unavailable"))
+        st.header("Key Milestones")
+        milestones = response.parameters.get("milestones", [])
+        st.markdown(
+            get_md_list(milestones),
+            unsafe_allow_html=True,
+        )
 
-    st.header("Key Milestones")
-    milestones = response.parameters.get("milestones", [])
-    st.text("\n".join(milestones))
+        st.header("Financial Performance")
+        st.markdown(
+            get_md_normal_text(
+                response.parameters.get("financial_performance", "Unavailable")
+            ),
+            unsafe_allow_html=True,
+        )
 
-    st.header("Financial Performance")
-    st.text(response.parameters.get("financial_performance", "Unavailable"))
+        st.header("Products and Services")
+        ps = response.parameters.get("products_and_services", [])
+        st.markdown(get_md_list(ps), unsafe_allow_html=True)
 
-    st.header("Products and Services")
-    ps = response.parameters.get("products_and_services", [])
-    st.text("\n".join(ps))
-
-    st.header("Funding Details")
-    st.text(response.parameters.get("funding", "Unavailable"))
-
-
-st.text_input("Company name or URL", key="company_id", on_change=execute)
+        st.header("Funding Details")
+        st.markdown(
+            get_md_normal_text(response.parameters.get("funding", "Unavailable")),
+            unsafe_allow_html=True,
+        )
